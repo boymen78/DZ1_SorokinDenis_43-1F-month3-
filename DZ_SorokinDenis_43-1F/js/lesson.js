9ba8b88054fcb12086e594d0c40484dfca31182f
boymen78 <denis03032004@gmail.com>
2024-11-17 19:25:47 +0600

    Add files via upload

--- DZ_SorokinDenis_43-1F/js/lesson.js
+++ DZ_SorokinDenis_43-1F/js/lesson.js
@@ -42,3 +42,50 @@ const autoSlide = () => {
 };
 
 setInterval(autoSlide, 3000);
+
+// CONVERTER
+
+const usdInput = document.querySelector("#usd");
+const somInput = document.querySelector("#som");
+const eurInput = document.querySelector("#eur");
+
+const converter = (element, targetElement1, targetElement2) => {
+    element.oninput = () => {
+        const request = new XMLHttpRequest();
+        request.open("GET", "../data/converter.json");
+        request.setRequestHeader("Content-type", "application/json");
+        request.send();
+
+        request.onload = () => {
+            const data = JSON.parse(request.response);
+
+            if (element.id === "som") {
+                targetElement1.value = (element.value / data.usd).toFixed(2);
+                targetElement2.value = (element.value / data.eur).toFixed(2);
+            }
+
+            if (element.id === "usd") {
+                targetElement1.value = (element.value * data.usd).toFixed(2);
+                targetElement2.value = ((element.value * data.usd) / data.eur).toFixed(
+                    2
+                );
+            }
+
+            if (element.id === "eur") {
+                targetElement1.value = (element.value * data.eur).toFixed(2);
+                targetElement2.value = ((element.value * data.eur) / data.usd).toFixed(
+                    2
+                );
+            }
+
+            if (element.value === "") {
+                targetElement1.value = "";
+                targetElement2.value = "";
+            }
+        };
+    };
+};
+
+converter(somInput, usdInput, eurInput);
+converter(usdInput, somInput, eurInput);
+converter(eurInput, somInput, usdInput);
